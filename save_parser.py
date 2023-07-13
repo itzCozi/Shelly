b78b0b86b9e011a5002f41fb57f4f77a8a71d82b
Cooper <Cooperransom08@gmail.com>
2023-07-13 19:52:27 -0400

    add buggy code lol

--- save_parser.py
+++ save_parser.py
@@ -67,3 +67,27 @@ def parseSave(file_path):
 
   print('\n'.join(lines))
 
+
+def editMoney(file_path, money):
+  if not os.path.exists(file_path):
+    raise FileNotFoundError('Given file path does not exist')
+  else:
+    with open(file_path, 'r') as r:
+      lines = r.read().split(',')
+      r.close()
+  
+  for line in lines:
+    if '"money":' in line:
+      linenum = lines.index(line)
+      linelist = list(lines[linenum])
+      money_amount = # The float of money converted to int
+      print(money_amount)
+      new = int(money_amount + money)
+      line = f'"money":{new},'
+    lines.append(line)
+  
+  #with open(file_path, 'w') as out:
+  #  out.write(''.join(lines)) ; out.close()
+
+editMoney('CS_Saves/savegame.save', 400)
+#parseSave('CS_Saves/savegame.save')
